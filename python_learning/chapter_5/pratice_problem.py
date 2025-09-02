9cd74e6e898b593725f84034afa4ee0c9cbe3a8b
microprixs <arpit.mishra@microprixs.in>
2025-09-02 15:47:51 +0530

    Sets and Dictionaries in Python

--- python_learning/chapter_5/pratice_problem.py
+++ python_learning/chapter_5/pratice_problem.py
@@ -9,9 +9,14 @@
 
 # print(english_translations["madad"])
 
-index = 0
-unque_nums = set()
+# index = 0
+# unque_nums = set()
 
-for index in range(7):
-    number = int(input("Enter Number: "))
-    unque_nums.add(number)
+# for index in range(7):
+#     number = int(input("Enter Number: "))
+#     unque_nums.add(number)
+
+
+# Sets and Dictionaries
+
+# Dictionary:- Dictionary are {key:value} pair. Sets are defined using {'hello','two'}. sets could only contain mutable and Hashable Items inside set. it contains only unique values and are not indexed.
