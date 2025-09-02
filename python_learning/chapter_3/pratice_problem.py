cf64e75d836094a4572023b747227560b2abd8f0
microprixs <arpit.mishra@microprixs.in>
2025-09-02 12:45:00 +0530

    Learn about strings and string methods and solved problems based on it

---
+++ python_learning/chapter_3/pratice_problem.py
@@ -0,0 +1,20 @@
+# Chapter 3 -> String Pratice Problem Sets
+
+# user_name = input("Enter User Name: ")
+# print(f"Good Afternoon: {user_name}")
+
+# letter = '''
+#     Dear Arpit,
+#     You are Selected
+#     25/08/2025
+# '''
+# print(letter)
+
+
+# Program to detect double space 
+
+# str_Chars = "Hello,  I am Arpit"
+# print(str_Chars.find("  "))
+
+
+# Strings are Immutable in the . It means strings can not be modified, In doing so you create a new string Instead.
