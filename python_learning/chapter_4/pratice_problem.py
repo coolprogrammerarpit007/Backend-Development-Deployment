2b5967bb4e58a8cbd362d34449293526b4953fc3
microprixs <arpit.mishra@microprixs.in>
2025-09-02 13:23:11 +0530

    Learn about Tuples and List data-structures in python and solved problems in it

---
+++ python_learning/chapter_4/pratice_problem.py
@@ -0,0 +1,48 @@
+# Python Data Structures Lists and Tuples
+
+# Lists are Mutable whereas Tuples are Immutables.
+# Tuples are comparitively much faster then Lists 
+# Tuples are recommended to use where data is constant 
+
+# planets = ("EARTH","MARS","VENUS","MURCURY","JUPYTER")
+# print(planets[0])
+
+# Practice Problems
+
+# fruits = []
+
+# index = 0
+# for index in range(7):
+#     fruit = input("Enter Fruit Name: ")
+#     fruits.append(fruit)
+
+
+# print(fruits)
+
+
+# student_marks = []
+
+# index = 0
+# for index in range(6):
+#     student_mark = int(input("Enter Student Marks out of 100: "))
+#     student_marks.append(student_mark)
+
+# print(student_marks)
+# print("Student Marks in the sorted order: ")
+# print(student_marks)
+
+# numbers = [12,23,78,95]
+# total = 0
+
+# for number in numbers:
+#     total += number
+
+
+# print(f"Total of all Numbers inside List: {total}")
+
+# count the number of zeros in the tuple
+
+# a = (7,0,8,0,0,9)
+
+# count_zeros = a.count(0)
+# print(f"Number of Zeros: {count_zeros}")
