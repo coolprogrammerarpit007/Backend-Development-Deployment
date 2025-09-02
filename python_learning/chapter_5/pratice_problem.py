fca58b6e746f787ac801599b625175987fad3486
microprixs <arpit.mishra@microprixs.in>
2025-09-02 14:01:18 +0530

    Learn about dict and sets in python

---
+++ python_learning/chapter_5/pratice_problem.py
@@ -0,0 +1,17 @@
+# Dictionary and Sets 
+
+# Practice Problems
+
+# english_translations = {
+#     "madad":"Help",
+#     "kursi":"Chair"
+# }
+
+# print(english_translations["madad"])
+
+index = 0
+unque_nums = set()
+
+for index in range(7):
+    number = int(input("Enter Number: "))
+    unque_nums.add(number)
