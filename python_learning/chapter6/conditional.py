1d7ee485f7d2c06fbfcda4b3a509ca8a2b8e1732
microprixs <arpit.mishra@microprixs.in>
2025-09-02 18:21:30 +0530

    conditional statments problem-sets

---
+++ python_learning/chapter6/conditional.py
@@ -0,0 +1,32 @@
+# Practice Problems on Conditionals
+
+# Program to find the greatest of four numbers entered by the user
+
+# number_list = []
+
+
+# index = 0
+# for index in range(4):
+#     number = int(input("Enter Number: "))
+#     number_list.append(number)
+
+
+# largest_number = max(number_list)
+# print(f"Largest Numbers Entered by User: {largest_number}")
+
+
+# Check SPAM Message
+
+# spam_messages = ["Make a lot of money","Buy Now","Subscribe This","Click This"]
+
+# message = "Hello Python, I am learning backend development and deployment! Buy Now"
+# for msg in spam_messages:
+#     if msg in message:
+#         print("************ SPAM MESSAGE ***********")
+#         print(msg)
+#         break
+
+#     else:
+#         print("General")
+
+
