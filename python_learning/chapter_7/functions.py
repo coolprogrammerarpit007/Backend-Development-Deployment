26fc4ccc91f44c528d34e26499053565036e97f4
microprixs <arpit.mishra@microprixs.in>
2025-09-03 12:21:34 +0530

    Functions and recursions

---
+++ python_learning/chapter_7/functions.py
@@ -0,0 +1,16 @@
+# Functions and Recursions
+
+# Recursion is a function which call Itself
+
+
+
+# def factorial(n):
+#     if n == 1 or n == 0:
+#         return 1
+#     return n * factorial(n-1)
+
+
+# number = int(input("Enter Number: "))
+# result = factorial(number)
+# print(f"Factorial of Number {number} is: {result}")
+
